ea4c9e5630ba7d3f66e17cc1c7e1cdeb2143c7ff
ahmet cetin <ahmetlaw@outlook.com>
2022-12-19 20:14:04 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import data from "./components/data";
 // import Hero from "./components/Hero";
 import Card from "./components/Card";
-import katie from "../src/img/katie.png";
+
 function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        img={item.img}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
+
   return (
     <div>
       <Navbar />
       {/* <Hero /> */}
-      <Card
-        img={katie}
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      {cards}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,7 @@ function Card(props) {
         <img src={star} alt="star" className="starphoto" />
         <span>{props.rating}</span>
         <span className="gray">{props.reviewCount}</span>
-        <span className="gray">{props.country}</span>
+        <span className="gray">{props.location}</span>
       </div>
       <p>{props.title}</p>
       <p>
